f53cc357e0992f65865d4963e107cb1c279679d6
panteleyR <panteley98@bk.ru>
2022-06-12 12:30:49 +0300

    first commit

---
+++ src/FilesystemInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lilith\Filesystem;
+
+use Generator;
+
+interface FilesystemInterface
+{
+    public function mkdir(string $path, int $permission, bool $recursive, mixed $context): void;
+    public function rm(string $path, bool $recursive): void;
+    public function cp(string $from, string $to): void;
+    public function mv(string $from, string $to): void;
+    public function ls(string $path): Generator;
+    public function copyDirectory(string $from, string $to): void;
+}
